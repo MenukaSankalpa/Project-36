3edf04938656da369cfdc4a1389b124e48d8464a
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-12 21:23:49 +0530

    imporove  keyword and accsekey

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+const acceskey = "pF_VXFMk6kK5BQal2ExTDYz2OVEQceoWx-wBQkk8b30";
+
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
+
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.ariaValueMax;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${acceskey}`;
+}
